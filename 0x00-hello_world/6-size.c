5e8164bfdbc3f1daeb0726a2762b3a61ed25fe97
Siraaj <sirjeftha@gmail.com>
2022-06-02 15:21:16 -0700

    changed variable names

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 /**
+ *main -Entry point
  *
+ *Return: Always 0(Success)
  *
  */
 int main(void)
 {
-int a = sizeof(char);
-int b = sizeof(int);
-int c = sizeof(long int);
-int d = sizeof(long long int);
-int e = sizeof(float);
+int x = sizeof(char);
+int y = sizeof(int);
+int z = sizeof(long int);
+int a = sizeof(long long int);
+int b = sizeof(float);
 
-printf("Size of char: %d byte(s)\n", a);
-printf("Size of int: %d byte(s)\n", b);
-printf("Size of long int: %d byte(s)\n", c);
-printf("Size of long long int: %d byte(s)\n", d);
-printf("Size of float: %d byte(s)\n", e);
+printf("Size of char: %d byte(s)\n", x);
+printf("Size of int: %d byte(s)\n", y);
+printf("Size of long int: %d byte(s)\n", z);
+printf("Size of long long int: %d byte(s)\n", a);
+printf("Size of float: %d byte(s)\n", b);
 return (0);
 }

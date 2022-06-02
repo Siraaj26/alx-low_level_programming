93a1ecbd8c912973dcc2b757d0fe95d423728c81
Siraaj <sirjeftha@gmail.com>
2022-06-02 15:24:55 -0700

    update to betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  int x = sizeof(char);
-  int y = sizeof(int);
-  int z = sizeof(long int);
-  int a = sizeof(long long int);
-  int b = sizeof(float);
+int x = sizeof(char);
+int y = sizeof(int);
+int z = sizeof(long int);
+int a = sizeof(long long int);
+int b = sizeof(float);
 
-  printf("Size of char: %d byte(s)\n", x);
-  printf("Size of int: %d byte(s)\n", y);
-  printf("Size of long int: %d byte(s)\n", z);
-  printf("Size of long long int: %d byte(s)\n", a);
-  printf("Size of float: %d byte(s)\n", b);
-  return (0);
+printf("Size of char: %d byte(s)\n", x);
+printf("Size of int: %d byte(s)\n", y);
+printf("Size of long int: %d byte(s)\n", z);
+printf("Size of long long int: %d byte(s)\n", a);
+printf("Size of float: %d byte(s)\n", b);
+return (0);
 }

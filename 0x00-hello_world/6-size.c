a0611359b5e8aa2f5f8b55777360caa20230174b
Siraaj <sirjeftha@gmail.com>
2022-06-02 15:04:30 -0700

    created first script for c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ *
+ *
+ */
+int main(void)
+{
+int a = sizeof(char);
+int b = sizeof(int);
+int c = sizeof(long int);
+int d = sizeof(long long int);
+int e = sizeof(float);
+
+printf("Size of char: %d byte(s)\n", a);
+printf("Size of int: %d byte(s)\n", b);
+printf("Size of long int: %d byte(s)\n", c);
+printf("Size of long long int: %d byte(s)\n", d);
+printf("Size of float: %d byte(s)\n", e);
+return (0);
+}

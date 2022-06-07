e67b379c4d7f976b87ef3534f792afe60127d223
Siraaj <sirjeftha@gmail.com>
2022-06-07 06:45:06 -0700

    ended comments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - entry point
  * Description: print alphabet in lower case 10 times followed by a new line
  * Return:void
-
+ */
 void print_alphabet_x10(void)
 {
 for (i= 97; i <=122; i+++)

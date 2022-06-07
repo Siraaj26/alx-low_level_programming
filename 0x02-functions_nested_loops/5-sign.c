2fd6f694e80e545f797e0c4a8b49ec53a7a4cd9e
Siraaj <sirjeftha@gmail.com>
2022-06-07 13:31:27 -0700

    added spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 /**
  *print_sign - entry point
  *@n: the integer value it recieves
- *Return 1 if n is 0 or positive, -1 return 0
-   */
-  int print_sign(int n)
+ *Return: 1 if n is 0 or positive, -1 return 0
+ */
+int print_sign(int n)
 {
 if (n > 0)
 {
@@ -14,7 +14,7 @@ return (1);
 else if (n == 0)
 {
 _putchar('0');
-return(0);
+return (0);
 }
 else
 {

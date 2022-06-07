cc98c8f6d47982316c38a1e59fc4eb97129e9348
Siraaj <sirjeftha@gmail.com>
2022-06-07 08:36:42 -0700

    changed to betty style code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-int i,count;
- for (count = 0; count < 10; count++)
-   {
-     for (i = 97; i <= 122; i++)
-       {
-	 _putchar(i);
-	   }
-     _putchar('\n');
-       }
+int i, count;
+for (count = 0; count < 10; count++)
+{
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
 }
 

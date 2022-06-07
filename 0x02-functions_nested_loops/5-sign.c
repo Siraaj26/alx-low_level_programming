775c41c3a6db92585a22f0d69321deb5035fe100
Siraaj <sirjeftha@gmail.com>
2022-06-07 13:20:45 -0700

    if positive return 1 and if negative return 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#rint_sign - entry point
+*@n: the integer value it recieves
+*Return 1 if n is 0 or positive, -1 return 0
+   */
+  int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return(0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+_putchar('\n');
+return (0);
+}

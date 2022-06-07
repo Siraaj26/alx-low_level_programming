54ba81fc4979407028202c5cd783d6dac6f3e95a
Siraaj <sirjeftha@gmail.com>
2022-06-07 03:43:18 -0700

    change script

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include "main.h"
 /**
  * main -entry point
+ * Description: prints _putchar
  * Return:Always 0 (success)
  */
 
 int main(void)
 {
-char a[]="_putchar"
-putchar('a');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 return (0);
 }

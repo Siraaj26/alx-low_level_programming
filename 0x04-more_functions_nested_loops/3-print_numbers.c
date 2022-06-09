c820258e2e4e8108af0713734e6db53609fa7a72
Siraaj <sirjeftha@gmail.com>
2022-06-09 11:11:27 -0700

    corrected variable name

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers 0 to 9
+ *
  * Return: number 0 to 9
  */
+
 void print_number(void)
 {
 int i = 0;
+
 while (i <= 9)
 {
-_putchar(a + '0');
+_putchar(i + '0');
 i++;
 }
 _putchar('\n');

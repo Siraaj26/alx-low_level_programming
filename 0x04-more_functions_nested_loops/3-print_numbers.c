105fd916cfa5a1cb465c00c95d25dc41e0552a3f
Siraaj <sirjeftha@gmail.com>
2022-06-09 10:42:53 -0700

    print numbers from o to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return: number 0 to 9
+ */
+void print_number(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}

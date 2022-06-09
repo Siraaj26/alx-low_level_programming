192a987f8b1e46bd81997925d619251fe0ed9fef
Siraaj <sirjeftha@gmail.com>
2022-06-09 06:26:39 -0700

    created script to check if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ * Description: check for uppercase character
+ *@c: variable checked
+ * Return: 1 if c is uppercase and 0 if other character
+ */
+int _isupper(int c)
+{
+if ((c > 'A') && (c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

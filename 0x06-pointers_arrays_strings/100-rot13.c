ca7c28b0ce19efd0cd9387938f6c749b7b65c275
Siraaj <sirjeftha@gmail.com>
2022-06-16 01:59:00 -0700

    encode a string with rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13-Function  that that encodes a string using rot13
+ * @str: string to be encoded
+ * Return: an encoded string
+ */
+char *rot13(char *str)
+{
+int a = 0;
+
+while (str[a])
+{
+while ((str[a] >= 'a' && str[a] <= 'z') || (str[a] >= 'A' && str[a] <= 'Z'))
+{
+if ((str[a] > 'm' && str[a] <= 'z') || (str[a] > 'M' && str[a] <= 'Z'))
+{
+str[a] -= 13;
+break;
+}
+
+str[a] += 13;
+break;
+}
+
+a++;
+}
+return (str);
+}

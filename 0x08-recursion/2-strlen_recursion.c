90503760c326bcfb22b4b0e00575e5f356253a75
Siraaj <sirjeftha@gmail.com>
2022-06-22 05:00:46 -0700

    remove whitespace line at the end

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _strlen_recursion - a function that returns the length of a string
  * @s: string tested
+ * Return: always 0 for success
  */
 int _strlen_recursion(char *s)
 {
@@ -11,6 +12,6 @@ return (0);
 }
 else
 {
-return(1 + _strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s + 1));
 }
 }

9279f4e776c785d0fba817fc2972bcff36c2bd21
Siraaj <sirjeftha@gmail.com>
2022-06-22 10:37:50 -0700

    changed variable

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,10 +7,10 @@
  */
 int _pow_recursion(int x, int y)
 {
+int a = (y - 1)
 if (y < 0)
 return (-1);
 if (y == 0)
 return (1);
-int p = (x * _pow_recursion(x, y - 1);
-return (p);
+return (x * _pow_recursion(x, a));
 }

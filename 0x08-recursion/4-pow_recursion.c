b93d05cabc2d697297784969c9937b59a80944e4
Siraaj <sirjeftha@gmail.com>
2022-06-22 10:28:32 -0700

    created new varaible to stay under 80 charactres per line

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,9 +7,11 @@
  */
 int _pow_recursion(int x, int y)
 {
+int r = x; /*r is the result*/
 if (y < 0)
 return (-1);
-if (y == 0)
+if (y >= 0)
 return (1);
-return (x * _pow_recursion(x, y - 1));
+r *= _pow_recursion(x, y - 1);
+return (r);
 }

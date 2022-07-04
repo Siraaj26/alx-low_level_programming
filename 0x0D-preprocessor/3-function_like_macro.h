4ec88c72f3b341f459cef314a67dc5d3f6363a43
Siraaj <sirjeftha@gmail.com>
2022-07-04 03:39:25 -0700

    added comments on argument x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,6 +5,7 @@
  * File: 3-function_like_macro.h
  * Description: a function-like macro ABS(x)
  *              that computes the absolute value of a number x.
+ * @x: the value computed
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))

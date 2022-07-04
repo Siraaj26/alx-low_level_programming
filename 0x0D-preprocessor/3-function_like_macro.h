9064b876516f6d22f0c74a10c19fcbd52add493e
Siraaj <sirjeftha@gmail.com>
2022-07-04 03:08:50 -0700

    added file 0 to 2 for h libaries

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * 3-function_like_macro
+ * Description: a function-like macro ABS(x) that computes the absolute value of a number x.
+ */
+
+#define 

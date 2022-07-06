36d0cf77da23eb0113a8726bbcf2ffa0f3832b84
Siraaj <sirjeftha@gmail.com>
2022-07-06 09:52:54 -0700

    added 0file and header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/**
+ * Desc: Header file contain prototypes for all functions
+ *       used in the 0x0F-function_pointers directory.
+ */
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif

404f79e99974c88cac4c4758ba5c6d8777ca2b44
Siraaj <sirjeftha@gmail.com>
2022-07-14 11:01:44 -0700

    changed script

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,19 @@
 
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+  int i = 0;
 
-while (h)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-
-else
-printf("[%d] %s\n", h->len, h->str);
-
-nodes++;
-h = h->next;
-}
-
-return (nodes);
+  while (h != NULL)
+    {
+      if (h->str == NULL)
+	{
+	  printf("[0] (nil\n");
+	  h = h->next;
+	  i++
+	    }
+      printf("[%d] %s\n", h->len, h->str);
+      h = h->next;
+      i++;
+    }
+  return (i);
 }
